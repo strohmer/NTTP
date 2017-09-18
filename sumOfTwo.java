920d6b149e5cf042cd56837ccd04d3b7450833db
Jason <crg.jason@gmail.com>
2017-09-18 18:30:47 -0400

    fix indenting on sumOfTwo.java

--- sumOfTwo.java
+++ sumOfTwo.java
@@ -1,13 +1,13 @@
 public static boolean sumOfTwo(int a[], int b[], int v) {
-		for (int x = 0; x < a.length; x++) {
-			for (int y = 0; y < b.length; y++) {
-				if (a[x] + b[y] == v) {
-					return true;
-				}
+	for (int x = 0; x < a.length; x++) {
+		for (int y = 0; y < b.length; y++) {
+			if (a[x] + b[y] == v) {
+				return true;
 			}
 		}
-		return false;
-		//O(ab)
+	}
+	return false;
+	//O(ab)
 }
 
 public static boolean sumOfTwo2(int a[], int b[], int v) {
